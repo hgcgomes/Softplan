be86d71a68c4668174143c200f9f9a7d63924c26
Henrique Gomes <hgcgomes.business@gmail.com>
2021-02-08 18:41:43 -0300

    Criando controller Financeiro e método GET taxa de juros.

---
+++ Softplan/fontes/Softplan.Api/Controllers/FinanceiroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Softplan.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FinanceiroController : ControllerBase
+    {
+        [HttpGet, Route("taxaJuros")]
+        public decimal TaxaJuros() => 0.01M;
+    }
+}
