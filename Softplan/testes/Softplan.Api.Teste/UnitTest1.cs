663b31296755954281bff17180b985c1ecfa1680
Henrique Gomes <hgcgomes.business@gmail.com>
2021-02-08 18:34:14 -0300

    Nova solução. Novo projeto de teste. TDD. Teste falhou.

---
+++ Softplan/testes/Softplan.Api.Teste/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Softplan.Api.Teste
+{
+    public class FinanceiroTeste
+    {
+        [Fact]
+        public void FinanceiroController_RequisicaoGetTaxaJuros_ValorZeroPontoZeroUm()
+        {
+            Assert.True(false);
+        }
+    }
+}
