b183e7ba980aaa9399284f3c40b2fc1cf12b0ab3
Henrique Gomes <hgcgomes.business@gmail.com>
2021-02-08 21:15:57 -0300

    Removendo endpoint

--- Softplan/fontes/Softplan.ApiUm/Controllers/FinanceiroController.cs
+++ Softplan/fontes/Softplan.ApiUm/Controllers/FinanceiroController.cs
@@ -15,25 +15,5 @@ namespace Softplan.Api.Controllers
         /// <returns>Taxa de Juros</returns>
         [HttpGet, Route("taxaJuros"), ProducesResponseType(typeof(decimal), 200)]
         public decimal TaxaJuros() => Juros.TaxaJurosPadrao;
-
-
-        /// <summary>
-        /// Calcula valor final dos juros compostos
-        /// </summary>
-        /// <param name="valorInicial">Valor inicial base par ao cálculo</param>
-        /// <param name="tempoMeses">Tempo em meses para cálculo</param>
-        /// <response code="200"></response>
-        /// <returns></returns>
-        [HttpGet, Route("calculaJuros"), ProducesResponseType(typeof(decimal), 200)]
-        public decimal CalculaJuros()
-        {
-            var valorInicial = Convert.ToDecimal(Request.Query["valorInicial"]);
-            var tempoMeses = Convert.ToInt32(Request.Query["tempoMeses"]);
-            var taxaJuros = TaxaJuros();
-
-            var valorFinalTruncado = Juros.CalculaJurosCompostos(taxaJuros, valorInicial, tempoMeses, truncarCasasDecimais: 2);
-
-            return valorFinalTruncado;
-        }
     }
 }
